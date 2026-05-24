ac10f5b3972f25bbb87610b8370675e9ca256673
Tiago Marques <tgpsmarques@gmail.com>
2026-05-24 21:26:45 +0100

    cleanup

--- soubi-master/fifo/error.c
+++
@@ -1,6 +0,0 @@
-#include "cs.h"
-extern int errno;
-//return string of last system call error code
-char * sys_err_str( void ){
-	return strerror(errno);
-}
--- soubi-master/fifo/v2/fifo.h
+++
@@ -1,3 +0,0 @@
-#define FIFO1 "/tmp/bruno_fifo.1"
-#define FIFO2 "/tmp/bruno_fifo.2"
-#define PERMS 0666
